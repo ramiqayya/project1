faf71812b20e735127ab70b03daefa77eaa94343
ramiqayya <rami.qayya@gmail.com>
2023-01-29 22:58:00 +0200

    add results for user's query

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>", views.get, name="get")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,63 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
+from django import forms
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Search Encyclopedia'}))
+
 
 def index(request):
+
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data["search"]
+            print(query)
+            print(util.list_entries())
+            results = []
+            for top in util.list_entries():
+                if query in top:
+                    results.append(top)
+
+            if query in util.list_entries():
+                return render(request, "encyclopedia/page.html", {
+                    "topic": util.get_entry(query),
+                    "form": form
+                })
+            elif results:
+                return render(request, "encyclopedia/results.html", {
+                    "form": form,
+                    "results": results
+
+                }
+
+                )
+
+                return render(request, "encyclopedia/query.html",)
+
+            return render(request, "encyclopedia/index.html", {
+                "form": form,
+                "entries": util.list_entries()
+            })
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": SearchForm()
     })
 
+
+def get(request, title):
+
+    topic = util.get_entry(title)
+    if not topic:
+        return render(request, "encyclopedia/error.html")
+
+    return render(request, "encyclopedia/page.html", {
+        "topic": topic
+    })
