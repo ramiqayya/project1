5bd4211cb4416a65fc350e013caabdeca4197b87
ramiqayya <rami.qayya@gmail.com>
2023-01-30 19:25:30 +0200

    Markdown to HTML conversion

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from random import randrange
+from markdown2 import Markdown
 from . import util
 
 from django import forms
+markdowner = Markdown()
 
 
 class SearchForm(forms.Form):
@@ -24,6 +26,7 @@ class EditForm(forms.Form):
 
 
 def index(request):
+    alltop = util.list_entries()
 
     if request.method == "POST":
         form = SearchForm(request.POST)
@@ -32,19 +35,21 @@ def index(request):
             print(query)
             print(util.list_entries())
             results = []
-            for top in util.list_entries():
+            for top in alltop:
                 if query in top:
                     results.append(top)
 
-            if query in util.list_entries():
+            if query in alltop:
                 return render(request, "encyclopedia/page.html", {
                     "topic": util.get_entry(query),
-                    "form": form
+                    "form": form,
+                    "random": alltop[randrange(len(alltop))]
                 })
             elif results:
                 return render(request, "encyclopedia/results.html", {
                     "form": form,
-                    "results": results
+                    "results": results,
+                    "random": alltop[randrange(len(alltop))]
 
                 }
 
@@ -52,32 +57,38 @@ def index(request):
 
             return render(request, "encyclopedia/index.html", {
                 "form": form,
-                "entries": util.list_entries()
+                "entries": util.list_entries(),
+                "random": alltop[randrange(len(alltop))]
+
             })
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-        "form": SearchForm()
+        "form": SearchForm(),
+        "random": alltop[randrange(len(alltop))]
     })
 
 
 def get(request, title):
-
+    alltop = util.list_entries()
     topic = util.get_entry(title)
     if not topic:
         return render(request, "encyclopedia/error.html", {
             "code": 404,
-            "message": "Topic is not found"
+            "message": "Topic is not found",
+            "random": alltop[randrange(len(alltop))]
 
         })
 
     return render(request, "encyclopedia/page.html", {
-        "topic": topic,
-        "title": title
+        "topic": markdowner.convert(topic),
+        "title": markdowner.convert(title),
+        "random": alltop[randrange(len(alltop))]
     })
 
 
 def edit(request, title):
+    alltop = util.list_entries()
 
     if request.method == "POST":
         etopic = EditForm(request.POST)  # etopic is the name of the form
@@ -89,28 +100,34 @@ def edit(request, title):
 
             return render(request, "encyclopedia/index.html", {
                 "entries": util.list_entries(),
-                "form": SearchForm()
+                "form": SearchForm(),
+                "random": alltop[randrange(len(alltop))]
             })
     content = util.get_entry(title)
     initial_date = {"title": title, "content": content}
 
     return render(request, "encyclopedia/edit.html", {
         "form3": EditForm(initial=initial_date),
-        "form": SearchForm()
+        "form": SearchForm(),
+        "random": alltop[randrange(len(alltop))]
     })
 
 
 def new(request):
+
+    alltop = util.list_entries()
 
     if request.method == "POST":
         topicf = NewForm(request.POST)  # topicf is the name of the form
         if topicf.is_valid():
             newt = topicf.cleaned_data["title"]  # extracting the title
             content = topicf.cleaned_data["content"]  # extracting the contact
-            if newt in util.list_entries():  # check if the title is already exists
+            if newt in alltop:  # check if the title is already exists
                 return render(request, "encyclopedia/error.html", {
                     "code": 403,
-                    "message": "This topic is already exists"
+                    "message": "This topic is already exists",
+                    "random": alltop[randrange(len(alltop))]
+
 
                 })
 
@@ -118,10 +135,12 @@ def new(request):
 
             return render(request, "encyclopedia/index.html", {
                 "entries": util.list_entries(),
-                "form": SearchForm()
+                "form": SearchForm(),
+                "random": alltop[randrange(len(alltop))]
             })
 
     return render(request, "encyclopedia/new.html", {
         "form2": NewForm(),
-        "form": SearchForm()
+        "form": SearchForm(),
+        "random": alltop[randrange(len(alltop))]
     })
